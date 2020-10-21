50efc6b4b1c970d13bbc5fb7b59735d3d108104a
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-21 20:41:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [paragraph,setParagraph] =React.useState("");
+  handleClick = (event) => {
+    setParagraph("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+           <Button id="para" onClick={(event) => handleClick(event)}>
+          para
+        </Button>
+        <p>{paragraph}</p>
     </div>
   );
 }
