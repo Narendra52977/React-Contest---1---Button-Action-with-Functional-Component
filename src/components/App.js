7e6f07cb8c8ea3f3596448a3ec6051a6d28bf9aa
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-21 20:50:35 +0530

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [paragraph,setParagraph] =React.useState("");
-  handleClick = (event) => {
-    setParagraph("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  let [paragraph, setParagraph] = React.useState("");
+  const handleClick = (event) => {
+    setParagraph(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
   };
   return (
     <div id="main">
-           <Button id="click" onClick={(event) => handleClick(event)}>
-          para
-        </Button>
-        <p id="para">{paragraph}</p>
+      <button id="click" onClick={(event) => handleClick(event)}>
+        para
+      </button>
+      <p id="para">{paragraph}</p>
     </div>
   );
 }
-
 
 export default App;
